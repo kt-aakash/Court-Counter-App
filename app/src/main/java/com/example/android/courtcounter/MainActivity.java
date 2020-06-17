a7d02797617f3bf08843a658e849078d6701a5ee
Aakash K T <ktaakash@gmail.com>
2020-06-17 16:46:55 +0530

    Overriding protected void onSaveInstanceState(@NonNull Bundle outState) method to save the states of the important variables and retrieve them after change in the orientation of the screen.
    This will prevent the resetting of values when user switches from
    potrait to landscape and vice versa.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.courtcounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,13 +9,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int teamA_score = 0;  // Initializing Global var to track Team A's score
-    int teamB_score = 0;  // Initializing Global var to track Team B's score
+    private int teamA_score = 0;  // Initializing Global var to track Team A's score
+    TextView teamA_TextView;      // Initializing respective TextView
+
+    private int teamB_score = 0;  // Initializing Global var to track Team B's score
+    private TextView teamB_TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teamA_TextView = (TextView) findViewById(R.id.textView_TeamA_Score);
+        teamB_TextView = (TextView) findViewById(R.id.textView_TeamB_Score);
+
+        if(savedInstanceState != null){
+
+            teamA_score = savedInstanceState.getInt("countA");   // Retrieving the state of the variable before the change in screen orientation
+            teamA_TextView.setText(String.valueOf(teamA_score));
+            teamB_score = savedInstanceState.getInt("countB");
+            teamB_TextView.setText(String.valueOf(teamB_score));
+        }
     }
     /**
      * This function adds three points on a three pointer for TeamA
@@ -76,17 +91,28 @@ public class MainActivity extends AppCompatActivity {
      * This method displays TeamA's score value on the screen.
      */
     private void displayTeamAScore() {
-        TextView textView_TeamA_Score = (TextView) findViewById(R.id.textView_TeamA_Score);
-        textView_TeamA_Score.setText(""+teamA_score);
+        teamA_TextView.setText(String.valueOf(teamA_score));
     }
 
     /**
      * This method displays TeamB's score value on the screen.
      */
     private void displayTeamBScore() {
-        TextView teamB_TextView = (TextView) findViewById(R.id.textView_TeamB_Score);
-        teamB_TextView.setText("" + teamB_score);
+        teamB_TextView.setText(String.valueOf(teamB_score));
     }
 
+    /**
+     *
+     * This method is being overrided inorder to save the variable states so as to retrieve them after change in screen orientatin
+     * The staes saved will then be accessed using the respectvie key values during the Oncreate() method execution
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("countA", teamA_score);
+        outState.putInt("countB", teamB_score);
+    }
 
 }
